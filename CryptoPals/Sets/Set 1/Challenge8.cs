aec646e6b6cd1ef36be5dd36d316668458da22e5
Cian <47340380+Schalasoft@users.noreply.github.com>
2020-07-16 05:42:18 +0100

    Update Challenge8.cs

--- CryptoPals/Sets/Set 1/Challenge8.cs
+++ CryptoPals/Sets/Set 1/Challenge8.cs
@@ -2,7 +2,6 @@
 using CryptoPals.Interfaces;
 using System;
 using System.Linq;
-using System.Text;
 
 namespace CryptoPals.Sets
 {
@@ -22,7 +21,6 @@ namespace CryptoPals.Sets
         // Reuse previous challenge functionality
         IChallenge1 challenge1 = (IChallenge1)ChallengeManager.GetChallenge((int)ChallengeEnum.Challenge1);
         IChallenge6 challenge6 = (IChallenge6)ChallengeManager.GetChallenge((int)ChallengeEnum.Challenge6);
-        IChallenge7 challenge7 = (IChallenge7)ChallengeManager.GetChallenge((int)ChallengeEnum.Challenge7);
 
         public string Solve(string input)
         {
@@ -30,7 +28,7 @@ namespace CryptoPals.Sets
             string[] lines = input.Split(Environment.NewLine);
 
             // Go through each line
-            int[] repeatedChunkCounts = new int[lines.Length];
+            int[] repeatedBlockCounts = new int[lines.Length];
             for(int i = 0; i < lines.Length; i++)
             {
                 // Get the line/hex as bytes
@@ -40,8 +38,8 @@ namespace CryptoPals.Sets
                 int size = 16;
                 byte[][] blocks = challenge6.CreateBlocks(bytes, size);
 
-                // Count the amount of repeated chunks we find
-                int repeatedChunkCount = 0;
+                // Count the amount of repeated blocks we find
+                int repeatedBlockCount = 0;
                 for (int j = 0; j < blocks.Length; j++)
                 {
                     for (int k = 0; k < blocks.Length; k++)
@@ -49,19 +47,20 @@ namespace CryptoPals.Sets
                         // Check if the block matches another block, exclude matching the block against itself
                         if (j != k && Enumerable.SequenceEqual(blocks[j], blocks[k]))
                         {
-                            repeatedChunkCount++;
+                            repeatedBlockCount++;
                         } 
                     }
                 }
 
-                // Add the amount of repeated chunks for this line to the array of line chunk counts (to find the line index)
-                repeatedChunkCounts[i] = repeatedChunkCount;
+                // Add the amount of repeated blocks for this line to an array (to find the line index)
+                repeatedBlockCounts[i] = repeatedBlockCount;
             }
 
-            // Get the AES encrypted line by getting the index of the line with the most repeated chunks
-            int mostRepetitions = repeatedChunkCounts.Max();
-            int encryptedLineIndex = Array.FindIndex(repeatedChunkCounts, x => x == mostRepetitions);
+            // Get the AES encrypted line by getting the index of the line with the most repeated block
+            int mostRepetitions = repeatedBlockCounts.Max();
+            int encryptedLineIndex = Array.FindIndex(repeatedBlockCounts, x => x == mostRepetitions);
 
+            // Format the output
             string output = $"{lines[encryptedLineIndex]}{Environment.NewLine}Index  : {encryptedLineIndex}{Environment.NewLine}Dupes  : {mostRepetitions}";
 
             return output;
