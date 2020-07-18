038d87ea42162d44aff4582f1cf3347c886b7d83
Cian <47340380+Schalasoft@users.noreply.github.com>
2020-07-18 09:24:47 +0100

    Update Program.cs

--- CryptoPals/Program.cs
+++ CryptoPals/Program.cs
@@ -83,7 +83,7 @@ namespace CryptoPals
                     input = inputAlias;
 
                 // Truncate the output to a max length
-                int maxLength = 1500;
+                int maxLength = 2000;
                 if (output.Length > maxLength)
                     output = $"{output.Substring(0, maxLength)} ...{Environment.NewLine}TRUNCATED";
 
