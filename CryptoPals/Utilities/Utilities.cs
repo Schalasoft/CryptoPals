07c5b8d5718ffc1838ed3ea51a774665a571f91c
Cian <47340380+Schalasoft@users.noreply.github.com>
2020-07-18 09:53:28 +0100

    Update Utilities.cs

--- CryptoPals/Utilities/Utilities.cs
+++ CryptoPals/Utilities/Utilities.cs
@@ -2,9 +2,17 @@
 
 namespace CryptoPals
 {
+    /// <summary>
+    /// A utility class used to convert bytes
+    /// </summary>
     class ByteConverter
     {
-        // Consume and return bytes from a memory stream
+        /// <summary>
+        /// Consume and return bytes from a memory stream
+        /// </summary>
+        /// <param name="stream">The memory stream to consume bytes from</param>
+        /// <param name="length">The number of bytes to consume</param>
+        /// <returns>A byte array containing the number of bytes specified from the current position of the memory stream</returns>
         public static byte[] GetBytes(MemoryStream stream, int length)
         {
             // Consume bytes from the memory stream
