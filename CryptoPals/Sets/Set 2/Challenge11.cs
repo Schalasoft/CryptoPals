daeab8614735be4a5c7548089677ef90e19c8ee9
Cian <47340380+Schalasoft@users.noreply.github.com>
2020-07-19 09:07:09 +0100

    Update Challenge11.cs

--- CryptoPals/Sets/Set 2/Challenge11.cs
+++ CryptoPals/Sets/Set 2/Challenge11.cs
@@ -1,9 +1,6 @@
 ﻿using CryptoPals.Enumerations;
 using CryptoPals.Interfaces;
-using Org.BouncyCastle.Crypto;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
 
 namespace CryptoPals.Sets
