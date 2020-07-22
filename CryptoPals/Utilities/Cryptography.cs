5231ed85742a0d2074743d157c74f82f5d7ae4ae
Cian <47340380+Schalasoft@users.noreply.github.com>
2020-07-22 20:37:10 +0100

    Create Cryptography.cs

---
+++ CryptoPals/Utilities/Cryptography.cs
@@ -0,0 +1,28 @@
+﻿using System.Security.Cryptography;
+
+namespace CryptoPals.Utilities
+{
+    static class Cryptography
+    {
+		public static byte[] AES_128_ECB_Encrypt(byte[] plainBytes, byte[] key)
+		{
+			var aes = new AesCryptoServiceProvider
+			{
+				KeySize = 128,
+				Key = key,
+				Mode = CipherMode.ECB,
+				Padding = PaddingMode.Zeros
+			};
+
+			var encryptor = aes.CreateEncryptor();
+			byte[] cipherBytes = new byte[plainBytes.Length];
+			for (int offset = 0; offset < plainBytes.Length; offset += aes.BlockSize / 8)
+			{
+				encryptor.TransformBlock(plainBytes, offset, aes.BlockSize / 8, cipherBytes, offset);
+			}
+
+			return cipherBytes;
+		}
+	}
+}
+
