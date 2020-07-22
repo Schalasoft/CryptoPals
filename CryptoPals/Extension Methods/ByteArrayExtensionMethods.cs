ce6950b3d4f11137a480008f622ff0c5a948c353
Cian <47340380+Schalasoft@users.noreply.github.com>
2020-07-22 20:14:50 +0100

    Added extension methods

---
+++ CryptoPals/Extension Methods/ByteArrayExtensionMethods.cs
@@ -0,0 +1,12 @@
+﻿using System.Text;
+
+namespace CryptoPals.Extension_Methods
+{
+    static class ByteArrayExtensionMethods
+    {
+        public static string ToASCIIString(this byte[] bytes)
+        {
+            return Encoding.ASCII.GetString(bytes);
+        }
+    }
+}
---
+++ CryptoPals/Extension Methods/StringExtensionMethods.cs
@@ -0,0 +1,12 @@
+﻿using System.Text;
+
+namespace CryptoPals.Extension_Methods
+{
+    static class StringExtensionMethods
+    {
+        public static byte[] ToBytes(this string text)
+        {
+            return Encoding.ASCII.GetBytes(text);
+        }
+    }
+}
