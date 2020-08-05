6f88d06a4bebe00072edf417b469141c2e19c0ec
Cian <47340380+Schalasoft@users.noreply.github.com>
2020-08-05 04:35:39 +0100

    Challenge 14

--- CryptoPals/Interfaces/Set 2/IChallenge14.cs
+++ CryptoPals/Interfaces/Set 2/IChallenge14.cs
@@ -1,5 +1,8 @@
 ﻿namespace CryptoPals.Interfaces
 {
+    /// <summary>
+    /// Byte-at-a-time ECB decryption (Harder)
+    /// </summary>
     public interface IChallenge14 : IChallenge
     {
 
--- CryptoPals/Sets/Set 2/Challenge14.cs
+++ CryptoPals/Sets/Set 2/Challenge14.cs
@@ -1,9 +1,29 @@
-﻿using CryptoPals.Interfaces;
+﻿using CryptoPals.Enumerations;
+using CryptoPals.Interfaces;
+using CryptoPals.Managers;
 
 namespace CryptoPals.Sets
 {
+    /// <inheritdoc cref="IChallenge14"/>
     class Challenge14 : IChallenge14, IChallenge
     {
+        /*
+        Byte-at-a-time ECB decryption (Harder)
+        Take your oracle function from #12. Now generate a random count of random bytes and prepend this string to every plaintext. You are now doing:
+
+        AES-128-ECB(random-prefix || attacker-controlled || target-bytes, random-key)
+        Same goal: decrypt the target-bytes.
+
+        Stop and think for a second.
+        What's harder than challenge #12 about doing this? How would you overcome that obstacle? The hint is: you're using all the tools you already have; no crazy math is required.
+
+        Think "STIMULUS" and "RESPONSE".
+        */
+
+        // Use previous challenge functionality
+        IChallenge13 challenge13 = (IChallenge13)ChallengeManager.GetChallenge((int)ChallengeEnum.Challenge13);
+
+        /// <inheritdoc/>
         public string Solve(string input)
         {
             return "";
